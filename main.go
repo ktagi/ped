32d2d0eb39cda1d10e9ff16f4fb16d75590e406e
Kenichi TAGI <tagi@jspnet.co.jp>
2021-09-29 14:48:10 +0900

    Refactor for testability

--- main.go
+++ main.go
@@ -4,31 +4,45 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"net/url"
 	"os"
 )
 
 func main() {
+	cli := &CLI{inStream: os.Stdin, outStream: os.Stdout, errStream: os.Stderr}
+	os.Exit(cli.Run(os.Args))
+}
+
+type CLI struct {
+	inStream             io.Reader
+	outStream, errStream io.Writer
+}
+
+func (c *CLI) Run(args []string) int {
 
 	var (
 		e bool
 	)
-	flag.BoolVar(&e, "e", false, "encode mode")
-	flag.Parse()
+	flags := flag.NewFlagSet(args[0], flag.ContinueOnError)
+	flags.BoolVar(&e, "e", false, "encode mode")
+	flags.Parse(args[1:])
 
-	sc := bufio.NewScanner(os.Stdin)
+	sc := bufio.NewScanner(c.inStream)
 	if !sc.Scan() {
-		os.Exit(0)
+		return 0
 	}
 
 	if e {
-		fmt.Println(url.PathEscape(sc.Text()))
+		fmt.Fprintln(c.outStream, url.PathEscape(sc.Text()))
 	} else {
 		u, err := url.PathUnescape(sc.Text())
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
+			fmt.Fprintln(c.errStream, err)
+			return 1
 		}
-		fmt.Println(u)
+		fmt.Fprintln(c.outStream, u)
 	}
+
+	return 0
 }
